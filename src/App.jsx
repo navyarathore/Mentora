7423a174b92379e1678e4670e509889570ef2e3c
Navya Rathore <rathorenavya45@gmail.com>
2025-04-20 07:03:30 +0530

    add: partners route

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,8 @@ import Support from './pages/Support';
 import Community from './pages/Community';
 import Pricing from './pages/Pricing';
 import Enterprise from './pages/Enterprise';
+import Partners from './pages/Partners';
+
 // OCConnect configuration
 const ocConnectOpts = {
   clientId: import.meta.env.VITE_OC_CLIENT_ID || 'sandbox',
@@ -149,6 +151,7 @@ function App() {
                   <Route path="community" element={<Community />} />
                   <Route path="pricing" element={<Pricing />} />
                   <Route path="enterprise" element={<Enterprise />} />
+                  <Route path="partners" element={<Partners />} />
                   <Route path="*" element={<NotFound />} />
                   <Route
                     path="/redirect"
